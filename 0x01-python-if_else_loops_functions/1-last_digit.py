e98572a6bd39ab41978cc53f178fa0daeb59c258
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-06 07:20:48 -0700

    Correction of format i used f instead of .format

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-n = number
+last_digit = abs(number) % 10
 if number < 0:
-    number = -number
-last_digit = number % 10
+    last_digit = -last_digit
+print("Last digit of {} is {} and is ".format(number, last_digit), end="")
 if last_digit > 5:
-    str_to_add = "and is greater than 5"
+    print("greater than 5")
 elif last_digit == 0:
-    str_to_add = "and is 0"
+    print("0")
 else:
-    str_to_add = "and is less than 6 and not 0"
-print(f"Last digit of {n} is {last_digit} {str_to_add}")
+    print("less than 6 and not 0")
