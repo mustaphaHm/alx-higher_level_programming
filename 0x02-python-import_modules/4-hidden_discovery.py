2ccbcaf798e454d44b543644bbf780c991e610f4
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-08 05:48:59 -0700

    Task 4,a program that prints all the names defined by the compiled module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
     array = dir(hidden_4)
     for element in array:
-        if not l.startswith("_"):
-            print(l)
+        if not element.startswith("_"):
+            print(element)
