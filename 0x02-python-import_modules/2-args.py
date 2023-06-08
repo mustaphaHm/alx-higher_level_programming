892549eccff0eded42fdf5f947ce06f0921f8902
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-08 05:00:56 -0700

    Task 2, if num of args = 0 i should print arguments. not arguments:

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     num_args = len(sys.argv) - 1
     if num_args == 1:
         print("{} argument:".format(num_args))
+    elif num_args == 0:
+        print("{} arguments.".format(num_args))
     else:
         print("{} arguments:".format(num_args))
 
