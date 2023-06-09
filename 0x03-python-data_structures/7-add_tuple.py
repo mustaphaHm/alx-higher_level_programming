d78c9b4651f1b307a5ad4f4fbdc6474fb73fba22
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-09 15:27:53 -0700

    Task 7,  a function that adds 2 tupleswq

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,14 @@
+def add_tuple(tuple_a=(), tuple_b=()):
+    lenTa = len(tuple_a)
+    lenTb = len(tuple_b)
+    for i in range(0, lenTa - 1):
+        if lenTa == 2 and lenTb == 2:
+            for j in range(0, lenTb - 1):
+                a = tuple_a[i] + tuple_b[j]
+                b = tuple_a[i + 1] + tuple_b[j + 1]
+                newT = (a, b)
+        elif lenTa == 2 and lenTb == 1:
+            newT = (tuple_a[i] + tuple_b[i], tuple_a[i + 1])
+        else:
+            newT = (tuple_a[i], tuple_a[i + 1])
+    return newT
