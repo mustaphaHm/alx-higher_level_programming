798220c577e4350ecf7c77997677780fcde7ce5b
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-10 05:07:56 -0700

    Task 7 , change the algorithme to pass teh code fails

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     lenTa = len(tuple_a)
     lenTb = len(tuple_b)
-    for i in range(0, lenTa - 1):
-        if lenTa == 2 and lenTb == 2:
-            for j in range(0, lenTb - 1):
-                a = tuple_a[i] + tuple_b[j]
-                b = tuple_a[i + 1] + tuple_b[j + 1]
-                newT = (a, b)
-        elif lenTa == 2 and lenTb == 1:
-            newT = (tuple_a[i] + tuple_b[i], tuple_a[i + 1])
+    if lenTa < 2:
+        if lenTa == 0:
+            tuple_a = 0, 0
         else:
-            newT = (tuple_a[i], tuple_a[i + 1])
-    return newT
+            tuple_a = tuple_a[0], 0
+    if lenTb < 2:
+        if lenTb == 0:
+            tuple_b = 0, 0
+        else:
+            tuple_b = tuple_b[0], 0
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
