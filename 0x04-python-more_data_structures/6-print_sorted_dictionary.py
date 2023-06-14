89c31f2984230b2f5476885b540aaf0a45eeda28
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-14 08:25:39 -0700

    Task 6, i changed the algorithm i sorted the keys first then retirive values

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    for el in sorted(a_dictionary):
+    dictKeys = list(a_dictionary.keys())
+    sortedKeys = sorted(dictKeys)
+    for el in sortedKeys:
         print("{} : {}".format(el, a_dictionary[el]))
