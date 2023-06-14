63c6251f8ad1122a2f4b3844c5fca2c89bff23a9
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-14 08:27:32 -0700

    Task 6, remove an added space by fault in the print

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     dictKeys = list(a_dictionary.keys())
     sortedKeys = sorted(dictKeys)
     for el in sortedKeys:
-        print("{} : {}".format(el, a_dictionary[el]))
+        print("{}: {}".format(el, a_dictionary[el]))
