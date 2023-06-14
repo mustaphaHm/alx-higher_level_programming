c8c05893d9f79ea0661b758758253fa3fb4fa5c1
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-14 16:07:24 -0700

    Task 12, adding a test if the string isinstance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,8 @@
-#!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if not roman_string:
+        return 0
+    elif not isinstance(roman_string, str):
         return 0
     else:
         rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
