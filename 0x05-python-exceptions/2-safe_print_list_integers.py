2cc190ddba4f47bca3d7dede1780ee52baed5ace
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-26 08:02:21 -0700

    tasks 2, correcting style issues

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     index = 0
     while index < x:
         try:
-            print("{:d}".format(my_list[index]), end ='')
+            print("{:d}".format(my_list[index]), end='')
             countInt += 1
-        except(ValueError , TypeError):
+        except(ValueError, TypeError):
             pass
         index += 1
     print()
