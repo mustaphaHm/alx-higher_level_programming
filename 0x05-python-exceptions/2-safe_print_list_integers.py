217c1a31935d1dc5d7364ee4524418a0c4f19944
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-26 08:59:09 -0700

    tasks 2, changing algorithme for rquirments

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    countInt = 0
-    index = 0
-    while index < x:
+    count = 0
+    for i in range(x):
         try:
-            print("{:d}".format(my_list[index]), end='')
-            countInt += 1
+            print("{:d}".format(my_list[i]), end='')
+            count += 1
         except(ValueError, TypeError):
-            pass
-        index += 1
+            continue
     print()
     return countInt
