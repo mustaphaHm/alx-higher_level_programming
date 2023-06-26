3ee2c28b8767fb5868bcf27faa829648a77178dd
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-26 06:24:37 -0700

    tasks 0 , changing the algorithm to fix checker errors check 12

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,13 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     try:
         count = 0
-        for el in my_list:
-            print(el, end='')
+        while count < x:
+            print(my_list[count], end='')
             count += 1
-            if count == x:
-                break
     except IndexError:
-        print("Index Error")
-    finally:
         print()
         return count
+    print()
+    return count
