1f3ab242a3406a24534719d3474d6e4ee12a4496
MustaphaHm <mustapha.houmam@gmail.com>
2023-06-27 14:41:53 -0700

    Task 2, i should check type of size then check the size if it's grater than 0 no the opposite

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,8 +17,8 @@ class Square:
             ValueError: if size < 0
             TypeError: if size is not an integer
         """
-        if size < 0:
-            raise ValueError("size must be >= 0")
-        elif not isinstance(size, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
