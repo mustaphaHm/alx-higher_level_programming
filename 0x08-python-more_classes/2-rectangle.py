ec5f9c2c3c1d04c39dbc96c0059c858cf08ec27d
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-03 09:22:11 -0700

    I forgot __ in the function perimeter :/

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -68,6 +68,6 @@ class Rectangle:
 
     def perimeter(self):
         """func to returns the perimeter of a rectangle"""
-        if(self.__width == 0 or self.__height == 0):
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
