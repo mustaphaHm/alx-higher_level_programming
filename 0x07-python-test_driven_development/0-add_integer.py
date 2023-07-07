4c0c519c3dca5a48ea9271f24f7063881181f3fd
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-07 07:19:32 -0700

    correct task 0 , code fails

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,12 +14,12 @@ def add_integer(a, b=98):
     Returns:
         int: the result of a + b
     """
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, (int | float)):
         raise TypeError("a must be an integer")
-    elif not isinstance(b, (int, float)):
+    elif not isinstance(b, (int | float)):
         raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
         b = int(b)
-    return a + b
+    return (a + b)
