2298b80870598abae6215bece2e63594771289d3
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-10 06:34:07 -0700

    Task 0, add docstring :)

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,4 +4,12 @@ attributes and methods of an object"""
 
 
 def lookup(obj):
+    """function that lists attributes and methods
+
+    Args:
+        obj: an object
+
+    Returns:
+        list: list of attributes and methods
+    """
     return dir(obj)
