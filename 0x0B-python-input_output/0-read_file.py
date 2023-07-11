4b1b458c03cc6131fa4e70d47d45430bdc97835f
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-11 05:17:01 -0700

    Task 0, a function that reads a text file (UTF8) and prints it to stdout:

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Definition of a func that reads a file"""
+
+
+def read_file(filename=""):
+    """function that reads a txt file
+
+    Args:
+        filename (str, optional): name of the file "".
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read())
