9e4c0eb28fc6371e54ee47506a5ba984deb5ab17
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-11 09:18:07 -0700

    the probleme is that i should make the file executbale :/

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,13 +8,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 file_name = "add_item.json"
-arguments = sys.argv[1:]
 obj = []
 try:
     obj = load_from_json_file(file_name)
 except FileNotFoundError:
     save_to_json_file([], file_name)
 
-obj.extend(arguments)
+for e in sys.argv[1:]:
+    obj.append(e)
 
 save_to_json_file(obj, file_name)
