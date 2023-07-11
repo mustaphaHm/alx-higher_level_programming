e37f327c6e799659fd2a3d2166a5e50dc08ada6d
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-11 06:36:19 -0700

    a function that returns the JSON representation of an object (string)

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Definition of func that returns
+the JSON representation of an object"""
+import json
+
+
+def to_json_string(my_obj):
+    """func that return json representation"""
+
+    json.dumps(my_obj)
