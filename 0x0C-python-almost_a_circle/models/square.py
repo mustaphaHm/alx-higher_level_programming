fd9e2bebb983e9cae95b4ca2b7490314fa3185bd
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-14 10:51:31 -0700

    Task 12, Assigns arguments to each attribut by order

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,3 +25,24 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assigns arguments to each attribut by order"""
+        if args:
+            lenghtARgs = len(args)
+            self.id = args[0] if lenghtARgs >= 1 else self.id
+            self.width = args[1] if lenghtARgs >= 2 else self.width
+            self.height = args[1] if lenghtARgs >= 2 else self.height
+            self.x = args[2] if lenghtARgs >= 3 else self.x
+            self.y = args[3] if lenghtARgs >= 4 else self.y
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.width = value
+                    self.height = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
