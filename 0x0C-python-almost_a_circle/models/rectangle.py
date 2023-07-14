0c557e6be7b0a36fc8e1b0e4f18a6a2ae354eba8
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-14 08:53:51 -0700

    Task 6, overriding the __str__ method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -76,3 +76,9 @@ class Rectangle(Base):
         """prints in stdout rectangle instance with char #"""
         for _ in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Overrinding str method"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.__x, self.__y, self.__width, self.__height
+                )
