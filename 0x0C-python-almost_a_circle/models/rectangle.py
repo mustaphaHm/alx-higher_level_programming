95394d71bbbe6cbf76b39da97690ca9c76d555c0
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-14 09:06:43 -0700

    Task 7, print the rectangle with the char # and handling x and y

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,11 +74,14 @@ class Rectangle(Base):
 
     def display(self):
         """prints in stdout rectangle instance with char #"""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
+            print(" " * self.__x, end="")
             print("#" * self.__width)
 
     def __str__(self):
         """Overrinding str method"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
-                self.id, self.__x, self.__y, self.__width, self.__height
-                )
+            self.id, self.__x, self.__y, self.__width, self.__height
+            )
