08cc6856abd02a3f28bfdf5feabeab3b4c23958f
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-14 14:17:38 -0700

    Task 17, add function that returns an instance with all attributes already set

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -46,3 +46,10 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        r = cls(1, 1)
+        r.update(**dictionary)
+        return r
