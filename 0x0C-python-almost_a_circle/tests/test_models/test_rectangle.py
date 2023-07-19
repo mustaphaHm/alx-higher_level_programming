67f9b067332d8a6d377b594ba1379ac4a2d6658b
MustaphaHm <mustapha.houmam@gmail.com>
2023-07-19 04:11:48 -0700

    forgot to put my tests inside the folder test_models

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" unit test for rectangle class """
+import unittest
+from models.rectangle import Rectangle
+from models.base import Base
+
+
+class TestRectangle(unittest.TestCase):
+    """ Definition of class rectangle """
+
+    def test_rectangle(self):
+        """ rectangle func """
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Rectangle(1, 2)
+        self.assertEqual(r3.id, 3)
+
+        r4 = Rectangle(1, 2, 3)
+        self.assertEqual(r4.id, 4)
+
+        r5 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r5.id, 5)
+
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2")
+
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, -2)
+
+        with self.assertRaises(ValueError):
+            Rectangle(0, -2)
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, -4)
+
+        self.assertEqual(Rectangle(3, 2).area(), 6)
+
+        self.assertEqual(
+                Rectangle(4, 6, 2, 1, 12).__str__(),
+                '[Rectangle] (12) 2/1 - 4/6'
+                )
+
+
+if __name__ == '__main__':
+    unittest.main()
