fd9d358be4e46a7b201d3103487dee850ce3a016
MustaphaHm <mustapha.houmam@gmail.com>
2023-09-11 08:48:53 -0700

    Task 2 , correct my variable name

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-const arguments = process.argv.length;
-console.log(arguments === 2 ? 'No argument' : (arguments === 3 ? 'Argument found' : 'Arguments found'))
-
+const args = process.argv.length;
+console.log(args === 2 ? 'No argument' : (args === 3 ? 'Argument found' : 'Arguments found'));
