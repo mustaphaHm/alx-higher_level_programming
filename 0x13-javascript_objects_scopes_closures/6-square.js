b49ae2b7c60b2a3021fac4ef4f0175d6fc99cbe1
MustaphaHm <mustapha.houmam@gmail.com>
2023-09-12 08:25:19 -0700

    Correct semistandars issues

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@ const Sq = require('./5-square');
 
 class Square extends Sq {
   charPrint (c = 'X') {
-    //const ch = c === undefined ? 'X' : 'C';
+    // const ch = c === undefined ? 'X' : 'C';
     for (let i = 0; i < this.height; i++) {
       let line = '';
       for (let j = 0; j < this.width; j++) {
