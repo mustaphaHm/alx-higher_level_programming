575bceaf2c111e4c61948eb840fb93c7f1ef6f7c
MustaphaHm <mustapha.houmam@gmail.com>
2023-09-15 09:46:43 -0700

    Task 2, Get state by name passed as argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Get state by name as argument."""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    STATE_NAME = sys.argv[4]
+
+    db = MySQLdb.connect(user=MY_USER, passwd=MY_PASS, db=MY_DB)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '"
+                + STATE_NAME + "' ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
